a6a3fb9a8f89bc72d8389730a0cc262e5dfe0fe4
MDJAmin <mohammadamindf@gmail.com>
2025-02-20 22:12:09 +0330

    Refactor: Get My elements from DOM

--- js/script.js
+++ js/script.js
@@ -25,3 +25,48 @@ const settings = {
 
 
   const blastHoleRadius = 18;
+
+// The main canvas element and its draw context
+const canvas = document.querySelector("#game");
+canvas.width = window.innerWidth * window.devicePixelRatio;
+canvas.height = window.innerHeight * window.devicePixelRatio;
+canvas.style.width = window.innerWidth + "px";
+canvas.style.height = window.innerHeight + "px";
+const ctx = canvas.getContext("2d");
+
+// Windmill elements
+const windmillDOM = document.querySelector("#windmill");
+const windmillHeadDOM = document.querySelector("#windmill-head");
+const windInfoDOM = document.querySelector("#wind-info");
+const windSpeedDOM = document.querySelector("#wind-speed");
+
+// Left info panel
+const info1DOM = document.querySelector("#info-left");
+const name1DOM = document.querySelector("#info-left .name");
+const angle1DOM = document.querySelector("#info-left .angle");
+const velocity1DOM = document.querySelector("#info-left .velocity");
+
+// Right info panel
+const info2DOM = document.querySelector("#info-right");
+const name2DOM = document.querySelector("#info-right .name");
+const angle2DOM = document.querySelector("#info-right .angle");
+const velocity2DOM = document.querySelector("#info-right .velocity");
+
+// Instructions panel
+const instructionsDOM = document.querySelector("#instructions");
+const gameModeDOM = document.querySelector("#game-mode");
+
+// The bomb's grab area
+const bombGrabAreaDOM = document.querySelector("#bomb-grab-area");
+
+// Congratulations panel
+const congratulationsDOM = document.querySelector("#congratulations");
+const winnerDOM = document.querySelector("#winner");
+
+// Settings toolbar
+const settingsDOM = document.querySelector("#settings");
+const singlePlayerButtonDOM = document.querySelectorAll(".single-player");
+const twoPlayersButtonDOM = document.querySelectorAll(".two-players");
+const autoPlayButtonDOM = document.querySelectorAll(".auto-play");
+const colorModeButtonDOM = document.querySelector("#color-mode");
+
