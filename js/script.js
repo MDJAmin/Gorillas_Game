ab961eb6bdc3f18aad51633addec3c9596b1215f
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-02-28 19:58:51 +0330

    Feat: drawGorilla
    render gorilla on designated buildings with detailed body parts

--- js/script.js
+++ js/script.js
@@ -451,3 +451,22 @@ function drawBuildings() {
     }
   });
 }
+
+function drawGorilla(player) {
+  ctx.save();
+
+  const building =
+    player === 1
+      ? state.buildings.at(1) // Second building
+      : state.buildings.at(-2); // Second last building
+
+  ctx.translate(building.x + building.width / 2, building.height);
+
+  drawGorillaBody();
+  drawGorillaLeftArm(player);
+  drawGorillaRightArm(player);
+  drawGorillaFace(player);
+  drawGorillaThoughtBubbles(player);
+
+  ctx.restore();
+}
