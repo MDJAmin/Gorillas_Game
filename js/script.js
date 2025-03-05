34b2d314efc5302c1650e3d296d6bc245706a84e
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-03-05 12:14:50 +0330

    Feat: singlePlayerButtonDOM
    Add event listener for single-player mode selection

--- js/script.js
+++ js/script.js
@@ -983,3 +983,14 @@ function announceWinner() {
   }
   showCongratulations();
 }
+
+singlePlayerButtonDOM.forEach((button) =>
+  button.addEventListener("click", () => {
+    settings.numberOfPlayers = 1;
+    gameModeDOM.innerHTML = "Player vs. Computer";
+    name1DOM.innerText = "Player";
+    name2DOM.innerText = "Computer";
+
+    newGame();
+  })
+);
